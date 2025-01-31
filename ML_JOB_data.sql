6080f4b63c613ae827c311fbdaf4a08b71187072
Chetana Rane <160422912+Chetana10r@users.noreply.github.com>
2025-01-31 16:03:37 +0530

    Adding files

---
+++ ML_JOB_data.sql
@@ -0,0 +1,10 @@
+use ML;
+
+create table job_desc10(Job_Id varchar(100),Experience varchar(200),Qualifications varchar(200),Salary_Range varchar(200),
+location varchar(200),Country varchar(200),Work_Type varchar(200),Company_Size int ,Job_Posting_Date date,Preference varchar(200),Contact_Person varchar(200),Contact varchar(100),
+Job_Title varchar(200),Role_j varchar(200),Job_Portal varchar(500),Job_Description varchar(500),
+Benefits varchar(500),skills varchar(500),Responsibilities varchar(500),Company varchar(500),Company_Profile varchar(500));
+
+desc job_desc10;
+
+select * from job_desc10;
