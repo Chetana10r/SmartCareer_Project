e9d2607403e33a8a6b038b56f274b06324f0d415
Chetana Rane <160422912+Chetana10r@users.noreply.github.com>
2025-04-04 21:04:39 +0530

    Adding the File

--- skills_recommender/src/index.js
+++ skills_recommender/src/index.js
@@ -1,11 +1,12 @@
+// index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css'; // You can use global styles here or import from ResumeRecommender.css if preferred.
-import ResumeRecommender from './ResumeRecommender';
+import './index.css';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ResumeRecommender />
+    <App />
   </React.StrictMode>
 );
